1a0d22871cf590e323e0cd74de752a4f32ee002a
hellomonikal <hellomonikal@163.com>
2025-10-23 16:37:18 +0800

    set up postgresql container

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,9 +9,9 @@ from app.config import settings
 from app.oauth2 import create_token
 from app import models
 
-SQLALCHEMY_DATABASE_URL  = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}_test'
+# SQLALCHEMY_DATABASE_URL  = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}_test'
 
-# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password@localhost:5432/fastapi_test'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password@localhost:5432/fastapi_test'
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
